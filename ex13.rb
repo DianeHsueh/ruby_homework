367cc478da8c4751e4a0c7c18680769ab3a9e60d
DianeHsueh <dia19910403@gmail.com>
2014-10-28 12:58:02 +0800

    ex13.rb

---
+++ ex13.rb
@@ -0,0 +1,8 @@
+first, second, third = ARGV
+first = "first"
+second = "2nd"
+third = "3rd"
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
