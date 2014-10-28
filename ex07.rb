597dccd26e2a11b5b0934ed2ae8e5bfd074ff277
DianeHsueh <dia19910403@gmail.com>
2014-10-28 10:47:10 +0800

    ex07.rb

---
+++ ex07.rb
@@ -0,0 +1,17 @@
+puts "Diane has some snacks"
+puts "one of  them is red as #{'apple'}" #Why not just type snow?
+puts "."* 12
+
+
+x1 = 'B'
+x2 = 'e'
+x3 = 'e'
+x4 = 'f'
+x5 = 'S'
+x6 = 't'
+x7 = 'e'
+x8 = 'a'
+x9 = 'k'
+
+print x1 + x2 + x3 + x4
+puts x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9
