6b2678de570238731b6fb495d3689c367d65f8d5
DianeHsueh <dia19910403@gmail.com>
2014-10-28 12:11:35 +0800

    ex11.rb

---
+++ ex11.rb
@@ -0,0 +1,10 @@
+print "how old are you? "
+age = gets.chomp
+print "how tall are you? "
+height = gets.chomp
+print "How much do you weigh? " 
+weight = gets.chomp
+print "how many days in Apr? "
+days = gets.chomp
+
+puts "so, you're #{age} old, #{height} tall and #{weight} heavy and #{days} days in Apr. "
