2d98d2b0837253dde64b8a023680241602734a68
DianeHsueh <dia19910403@gmail.com>
2014-10-28 12:04:03 +0800

    ex10.rb

---
+++ ex09.rb
@@ -0,0 +1,14 @@
+#here is some new strange stuff, remeber type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three bouble-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+"""
